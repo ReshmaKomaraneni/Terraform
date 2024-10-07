2819e590c1f077cbeccc2f20e0ad2369dbf50a55
ReshmaKomaraneni <radhakomaraneni@gmail.com>
2024-10-07 11:51:10 +0530

    create subnets

--- vpc.tf
+++ vpc.tf
@@ -7,3 +7,33 @@ resource "aws_vpc" "ecomm-app" {
     Name = "ecomm-app"
   }
 }
+#create subnet
+#web subnet
+resource "aws_subnet" "ecomm-web-sn" {
+  vpc_id     = aws_vpc.ecomm-app.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "ecomm-web-subnet"
+  }
+}
+
+#api subnet
+resource "aws_subnet" "ecomm-api-sn" {
+  vpc_id     = aws_vpc.ecomm-app.id
+  cidr_block = "10.0.2.0/25"
+
+  tags = {
+    Name = "ecomm-api-subnet"
+  }
+}
+
+#database subnet
+resource "aws_subnet" "ecomm-db-sn" {
+  vpc_id     = aws_vpc.ecomm-app.id
+  cidr_block = "10.0.3.0/26"
+
+  tags = {
+    Name = "ecomm-database-subnet"
+  }
+}
