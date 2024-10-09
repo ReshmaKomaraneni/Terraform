ea143793bb3ef5f46b4abb7c3ff782db49c7eaf6
ReshmaKomaraneni <radhakomaraneni@gmail.com>
2024-10-09 13:38:52 +0530

    updated keypair name

--- EC2.tf
+++ EC2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ecomm-web-server"{
     ami           = "ami-005fc0f236362e99f"
     instance_type = "t2.micro"
     subnet_id     = aws_subnet.ecomm-web-sn.id
-    key_name      = "resh.pem"
+    key_name      = "resh"
     vpc_security_group_ids = [aws_security_group.ecomm-web-sg.id]
     user_data = file("setup.sh")
       tags = {
