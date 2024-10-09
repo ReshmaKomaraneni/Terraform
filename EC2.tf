685c18ca1d02b2aeb4fda52722db531ad68125ab
ReshmaKomaraneni <radhakomaraneni@gmail.com>
2024-10-09 13:34:04 +0530

    updated server name

--- EC2.tf
+++ EC2.tf
@@ -1,5 +1,5 @@
 #Web Sever EC2
-resource "aws_instance" "ecomm-web=server"{
+resource "aws_instance" "ecomm-web-server"{
     ami           = "ami-005fc0f236362e99f"
     instance_type = "t2.micro"
     subnet_id     = aws_subnet.ecomm-web-sn.id
